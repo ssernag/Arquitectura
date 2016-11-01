ed49a22a3a10450e406a8405b20f8ccdb5c6153d
Sebastian Serna <sebastianserna.gomez@gmail.com>
2016-11-01 01:40:04 -0500

    Procesador 3 Sin conexiones

---
+++ Procesador3/tb_MUX.vhd
@@ -0,0 +1,78 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   01:31:48 11/01/2016
+-- Design Name:   
+-- Module Name:   C:/Users/CBAS/Desktop/UTP/Laboratorio de electronica/EJERCICIOS/XILINX PROYECTOS 20152 NOVIEMBRE 07/Procesador3/tb_MUX.vhd
+-- Project Name:  Procesador3
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Multiplexor
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_MUX IS
+END tb_MUX;
+ 
+ARCHITECTURE behavior OF tb_MUX IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Multiplexor
+    PORT(
+         i : IN  std_logic;
+         Out_SEU : IN  std_logic_vector(31 downto 0);
+         Crs2 : IN  std_logic_vector(31 downto 0);
+         Out_MUX : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal i : std_logic := '0';
+   signal Out_SEU : std_logic_vector(31 downto 0) := (others => '0');
+   signal Crs2 : std_logic_vector(31 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal Out_MUX : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   constant <clock>_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Multiplexor PORT MAP (
+          i => i,
+          Out_SEU => Out_SEU,
+          Crs2 => Crs2,
+          Out_MUX => Out_MUX
+        );
+
+   -- Clock process definitions
+   <clock>_process :process
+   begin
+		
+END;
---
+++ Procesador3/tb_PSR.vhd
@@ -0,0 +1,96 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   01:33:28 11/01/2016
+-- Design Name:   
+-- Module Name:   C:/Users/CBAS/Desktop/UTP/Laboratorio de electronica/EJERCICIOS/XILINX PROYECTOS 20152 NOVIEMBRE 07/Procesador3/tb_PSR.vhd
+-- Project Name:  Procesador3
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: PSR
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_PSR IS
+END tb_PSR;
+ 
+ARCHITECTURE behavior OF tb_PSR IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT PSR
+    PORT(
+         CLK : IN  std_logic;
+         Reset : IN  std_logic;
+         NZVC : IN  std_logic_vector(3 downto 0);
+         Carry : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal CLK : std_logic := '0';
+   signal Reset : std_logic := '0';
+   signal NZVC : std_logic_vector(3 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal Carry : std_logic;
+
+   -- Clock period definitions
+   constant CLK_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: PSR PORT MAP (
+          CLK => CLK,
+          Reset => Reset,
+          NZVC => NZVC,
+          Carry => Carry
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for CLK_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
